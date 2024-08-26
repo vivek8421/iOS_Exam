5dbb9edf4e798a18c84f7405739f6b03db7940cc
vivek8421 <vivek.lokhande@neosoftmail.com>
2024-08-26 13:10:05 +0530

    Add files via upload

---
+++ iOS_Exam/iOS_Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Exam
+//
+//  Created by Neosoft on 26/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
